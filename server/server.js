d29ffc6fe98583ee8565644bfa3b25f5a1fe4f04
Ryan Silva <RyandotSilva@gmail.com>
2019-08-17 11:24:02 -0400

    Add cors & body parser to server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("../server/schema/schema");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 	res.send("Server is up!");
