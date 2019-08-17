44eb524b0892ac8c83cf2d4668a504f4c248d9d8
Ryan Silva <RyandotSilva@gmail.com>
2019-08-17 11:52:55 -0400

    Add material UI

--- client/src/components/NewMovies.js
+++ client/src/components/NewMovies.js
@@ -2,14 +2,26 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { getNewMovies } from "../queries/queries";
 
+import { Link } from "react-router-dom";
+
 const NewMovies = props => {
 	const { loading, data } = useQuery(getNewMovies);
 
 	if (loading) return <h1>Loading...</h1>;
 
+	const renderMovies = () => {
+		return data.newMovies.map(movie => {
+			return (
+				<li key={movie.id}>
+					<Link to={""}>{movie.title}</Link>
+				</li>
+			);
+		});
+	};
+
 	console.log(data);
 
-	return <h1>NEW MOVIES!</h1>;
+	return <ul>{renderMovies()}</ul>;
 };
 
 export default NewMovies;
