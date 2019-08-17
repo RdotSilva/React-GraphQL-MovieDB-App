63a79d75e38bcd4f6d90bd74fdba663d69d7399b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-17 12:56:38 -0400

    Update movie card to show movie data

--- client/src/components/NewMovies.js
+++ client/src/components/NewMovies.js
@@ -1,29 +1,40 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { getNewMovies } from "../queries/queries";
-
 import MovieCard from "../components/MovieCard";
-
 import { Link } from "react-router-dom";
+
+// Material UI
+import Box from "@material-ui/core/Box";
 
 const NewMovies = props => {
 	const { loading, data } = useQuery(getNewMovies);
 
 	if (loading) return <h1>Loading...</h1>;
 
-	// const renderMovies = () => {
-	// 	return data.newMovies.map(movie => {
-	// 		return (
-	// 			<li key={movie.id}>
-	// 				<Link to={""}>{movie.title}</Link>
-	// 			</li>
-	// 		);
-	// 	});
-	// };
+	const renderMovies = () => {
+		return data.newMovies.map(movie => {
+			return (
+				<li key={movie.id}>
+					<MovieCard movieData={movie} />
+				</li>
+			);
+		});
+	};
 
-	console.log(data);
-
-	return <MovieCard />;
+	return (
+		<div style={{ width: "100%" }}>
+			<Box
+				display="flex"
+				p={1}
+				bgcolor="background.paper"
+				justifyContent="center"
+				flexWrap="wrap"
+			>
+				{renderMovies()}
+			</Box>
+		</div>
+	);
 };
 
 export default NewMovies;
