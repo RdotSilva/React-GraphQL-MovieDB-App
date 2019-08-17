b7172a97a98092e554fa5f1cc2e4163b586274bc
Ryan Silva <RyandotSilva@gmail.com>
2019-08-17 09:59:45 -0400

    Add express/graphql to server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./server/schema/schema");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+	res.send("Server is up!");
+});
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
