35cb42e394c61df5d6d4e92a10c8dc4ad94b78fd
Ryan Silva <RyandotSilva@gmail.com>
2019-08-17 11:12:38 -0400

    Add apollo client & provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-function App() {
-	return <h1>App works</h1>;
-}
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql"
+});
+
+const App = () => (
+	<ApolloProvider client={client}>
+		<h1>App works</h1>
+	</ApolloProvider>
+);
 
 export default App;
