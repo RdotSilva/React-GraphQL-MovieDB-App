cbc031afe92b55f7e5eaeefbe7dd21f95a1f5f9e
Ryan Silva <RyandotSilva@gmail.com>
2019-08-19 15:19:15 -0400

    Add useQuery hook with getMovieInfo

--- client/src/components/MovieSearch.js
+++ client/src/components/MovieSearch.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { getMovieInfo } from "../queries/queries";
+import MovieCard from "../components/MovieCard";
+import { Link } from "react-router-dom";
 
-const MovieSearch = () => {
+// Material UI
+import Box from "@material-ui/core/Box";
+
+const MovieSearch = props => {
+	const { loading, data } = useQuery(getMovieInfo);
+
+	if (loading) return <h1>Loading...</h1>;
 	return <div />;
 };
 
