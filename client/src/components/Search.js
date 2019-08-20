b8fb2fc1eeb6896d7b77dcd58de06d37b4fc525a
Ryan Silva <RyandotSilva@gmail.com>
2019-08-20 16:06:10 -0400

    Add handleInputChange function

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 const Search = props => {
 	const [searchValue, setSearchValue] = useState("");
 
+	const handleInputChange = e => {
+		setSearchValue(e.target.value);
+	};
+
 	return <div />;
 };
 
