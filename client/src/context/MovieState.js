e9afd8470cf630638bfa7ed4add5e7110875a76b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-21 19:25:14 -0400

    Add useQuery hook for search movies

--- client/src/context/MovieState.js
+++ client/src/context/MovieState.js
@@ -1,6 +1,10 @@
 import React, { useReducer } from "react";
 import axios from "axios";
+import MovieContext from "./movieContext";
+import MovieReducer from "./movieReducer";
 import keys from "../../../keys/devKeys";
+import { useQuery } from "@apollo/react-hooks";
+import { SEARCH_MOVIES } from "../queries/queries";
 
 const MovieState = props => {
 	const initialState = {
@@ -8,6 +12,10 @@ const MovieState = props => {
 		searchMovies: [],
 		loading: false
 	};
-};
 
-const [state, dispatch] = useReducer(MovieReducer, initialState);
+	const [state, dispatch] = useReducer(MovieReducer, initialState);
+
+	const { loading, data } = useQuery(SEARCH_MOVIES, {
+		variables: { searchField: searchValue }
+	});
+};
