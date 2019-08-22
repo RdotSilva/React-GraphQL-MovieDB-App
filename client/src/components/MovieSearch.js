8e344239a71c4c32da8b4f7f53d4b37d86d37330
Ryan Silva <RyandotSilva@gmail.com>
2019-08-22 15:34:32 -0400

    Update search bar styles

--- client/src/components/MovieSearch.js
+++ client/src/components/MovieSearch.js
@@ -41,9 +41,14 @@ const MovieSearch = props => {
 
 	return (
 		<div style={{ width: "100%", backgroundColor: "#ecf2f9" }}>
-			<form>
+			<form style={{ display: "block", textAlign: "center", margin: 10 }}>
 				<input value={searchValue} onChange={handleInputChange} type="text" />
-				<input onClick={callSearch} type="submit" value="Search" />
+				<input
+					style={{ margin: 8 }}
+					onClick={callSearch}
+					type="submit"
+					value="Search"
+				/>
 			</form>
 			<Box
 				display="flex"
