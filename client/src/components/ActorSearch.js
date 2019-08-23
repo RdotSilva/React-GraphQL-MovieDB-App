878f07561aafef5eacb829ba0f65f60f0aff4c2f
Ryan Silva <RyandotSilva@gmail.com>
2019-08-23 15:37:33 -0400

    Add handleInputChange function

--- client/src/components/ActorSearch.js
+++ client/src/components/ActorSearch.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 const ActorSearch = () => {
 	const [searchValue, setSearchValue] = useState(" ");
 
+	const handleInputChange = e => {
+		setSearchValue(e.target.value);
+	};
+
 	return <h1>Actor List</h1>;
 };
 
