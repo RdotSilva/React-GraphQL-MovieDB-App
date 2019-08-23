31139a3815ade0c3c52f9d04121843e01f21c657
Ryan Silva <RyandotSilva@gmail.com>
2019-08-23 15:39:29 -0400

    Add search actor input field

--- client/src/components/ActorSearch.js
+++ client/src/components/ActorSearch.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
-const ActorSearch = () => {
+// Material UI
+import Box from "@material-ui/core/Box";
+
+const ActorSearch = props => {
 	const [searchValue, setSearchValue] = useState(" ");
 
 	const handleInputChange = e => {
@@ -17,7 +20,28 @@ const ActorSearch = () => {
 		resetInputField();
 	};
 
-	return <h1>Actor List</h1>;
+	return (
+		<div style={{ width: "100%", backgroundColor: "#ecf2f9" }}>
+			<form style={{ display: "block", textAlign: "center" }}>
+				<input value={searchValue} onChange={handleInputChange} type="text" />
+				<input
+					style={{ margin: 8 }}
+					onClick={callSearch}
+					type="submit"
+					value="Search"
+				/>
+			</form>
+			<Box
+				display="flex"
+				p={1}
+				bgcolor="#ecf2f9"
+				justifyContent="center"
+				flexWrap="wrap"
+			>
+				{/* {loading ? <h1> </h1> : renderMovies()} */}
+			</Box>
+		</div>
+	);
 };
 
 export default ActorSearch;
