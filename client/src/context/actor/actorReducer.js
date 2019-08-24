f1ee3dfb060363994209dd95327975a43a400bda
Ryan Silva <RyandotSilva@gmail.com>
2019-08-24 13:07:51 -0400

    Add search actors case

--- client/src/context/actor/actorReducer.js
+++ client/src/context/actor/actorReducer.js
@@ -2,6 +2,13 @@ import { SEARCH_ACTORS, SET_LOADING } from "./types";
 
 export default (state, action) => {
 	switch (action.type) {
+		case SEARCH_ACTORS:
+			return {
+				...state,
+				actors: action.payload,
+				loading: false
+			};
+
 		default:
 			return state;
 	}
